1db4d8070eeec1570ef8e931aa896a9b6a7fdc47
Unnati-Malik <unnatimalik7@gmail.com>
2025-06-11 00:13:42 +0530

    Started the Project

---
+++ client/src/components/BlogCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const BlogCard = ({blog}) => {
+    const {title, description, category, image, id} = blog;
+  return (
+    <div>BlogCard</div>
+  )
+}
+
+export default BlogCard
